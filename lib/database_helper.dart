658d385ee2039bbbc3103cc186deec2869629ec5
Billy Yanez <bemonio@gmail.com>
2024-02-23 19:58:57 -0500

    feat: I'm Batman

---
+++ lib/database_helper.dart
@@ -0,0 +1,61 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper.internal();
+  factory DatabaseHelper() => _instance;
+
+  static Database? _db;
+
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db!;
+    }
+    _db = await initDb();
+    return _db!;
+  }
+
+  DatabaseHelper.internal();
+
+  Future<Database> initDb() async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, 'brinks.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database db, int version) async {
+        await db.execute('''
+          CREATE TABLE qr_data(
+            id INTEGER PRIMARY KEY,
+            datetime TEXT,
+            latitude TEXT,
+            longitude TEXT,
+            qr TEXT
+          )
+        ''');
+      },
+    );
+  }
+
+  Future<int> insertData(Map<String, dynamic> data) async {
+    final dbClient = await db;
+    return await dbClient.insert('qr_data', data);
+  }
+
+  Future<List<Map<String, dynamic>>> getData() async {
+    final dbClient = await db;
+    return await dbClient.query(
+      'qr_data',
+      orderBy: 'datetime DESC',
+    );
+  }
+
+  Future<void> resetDatabase() async {
+    final dbClient = await db;
+    await dbClient.close();
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, 'brinks.db');
+    await deleteDatabase(path);
+    _db = null;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,148 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:app/database_helper.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final dbHelper = DatabaseHelper();
+  await dbHelper.initDb();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Brinks - App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'QR Scanner'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final DatabaseHelper _databaseHelper = DatabaseHelper();
+
+  @override
+  void initState() {
+    super.initState();
+    requestLocationPermission();
+  }
+
+  Future<void> requestLocationPermission() async {
+    final status = await Permission.location.request();
+    if (status.isGranted) {
+      // Permisos concedidos, puedes acceder a la ubicación
+      // print('Permisos de ubicación concedidos');
+    } else if (status.isDenied) {
+      // El usuario negó los permisos, puedes mostrar un mensaje para solicitarlos nuevamente
+      // print('Permisos de ubicación denegados');
+    } else if (status.isPermanentlyDenied) {
+      // El usuario negó permanentemente los permisos, puedes redirigirlo a la configuración de la aplicación para habilitarlos manualmente
+      // print('Permisos de ubicación denegados permanentemente');
+    }
+  }
+
+  void showScanQr(BuildContext context) async {}
+
+  void _addRow() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    final latitude = position.latitude;
+    final longitude = position.longitude;
+    final now = DateTime.now();
+    final random = Random();
+    final qr = random.nextInt(999999);
+
+    await _databaseHelper.insertData({
+      'datetime': now.toIso8601String(),
+      'latitude': latitude.toStringAsFixed(6),
+      'longitude': longitude.toStringAsFixed(6),
+      'qr': qr.toString(),
+    });
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: FutureBuilder<List<Map<String, dynamic>>>(
+            future: _databaseHelper.getData(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else {
+                final dataList = snapshot.data!;
+                return DataTable(
+                  columns: const [
+                    DataColumn(label: Text('Fecha y hora')),
+                    DataColumn(label: Text('Latitud')),
+                    DataColumn(label: Text('Longitud')),
+                    DataColumn(label: Text('QR')),
+                  ],
+                  rows: dataList
+                      .map((data) => DataRow(cells: [
+                            DataCell(Text(data['datetime'])),
+                            DataCell(Text(data['latitude'])),
+                            DataCell(Text(data['longitude'])),
+                            DataCell(Text(data['qr'])),
+                          ]))
+                      .toList(),
+                );
+              }
+            },
+          ),
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () async {
+              await _databaseHelper.resetDatabase();
+              setState(() {});
+            },
+            tooltip: 'Reset Database',
+            child: const Icon(Icons.refresh),
+          ),
+          const SizedBox(width: 16),
+          FloatingActionButton(
+            onPressed: () {
+              _addRow();
+            },
+            tooltip: 'Scan QR',
+            child: const Icon(Icons.camera_alt),
+          ),
+        ],
+      ),
+    );
+  }
+}
