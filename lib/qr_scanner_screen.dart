6b7db4faf23f2b26555ad70bbf28a1124b425b57
Billy Yanez <bemonio@gmail.com>
2024-02-26 20:35:08 -0500

    refactor: QRScannerScreen

---
+++ lib/qr_scanner_screen.dart
@@ -0,0 +1,185 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:app/database_helper.dart';
+
+class QRScannerScreen extends StatefulWidget {
+  const QRScannerScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _QRViewExampleState();
+}
+
+class _QRViewExampleState extends State<QRScannerScreen> {
+  final DatabaseHelper _databaseHelper = DatabaseHelper();
+
+  Barcode? result;
+  QRViewController? controller;
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  bool cameraPaused = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // Activate QR acquisition automatically
+    resumeQRScanning();
+  }
+
+  Future<void> resumeQRScanning() async {
+    // Make sure the controller is initialized before calling resumeCamera()
+    if (controller != null) {
+      // Resume QR acquisition
+      await controller!.resumeCamera();
+      // Update the state to reflect that the camera has been resumed
+      setState(() {
+        cameraPaused = false;
+      });
+    }
+  }
+
+  Future<void> _addRow(String qrData, BarcodeFormat qrType) async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    final latitude = position.latitude;
+    final longitude = position.longitude;
+    final now = DateTime.now();
+    final qr = qrData;
+
+    await _databaseHelper.insertData({
+      'datetime': now.toIso8601String(),
+      'latitude': latitude.toStringAsFixed(6),
+      'longitude': longitude.toStringAsFixed(6),
+      'qr_data': qr,
+      'qr_type': qrType.name
+    });
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(flex: 4, child: _buildQrView(context)),
+          Expanded(
+            flex: 1,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: FittedBox(
+                fit: BoxFit.contain,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                        if (controller != null && !cameraPaused) {
+                          controller!.pauseCamera();
+                          setState(() {
+                            cameraPaused = true;
+                          });
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.all(16),
+                        shape: const CircleBorder(),
+                      ),
+                      child: const Icon(Icons.arrow_back),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        await controller?.toggleFlash();
+                        setState(() {});
+                      },
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.all(16),
+                        shape: const CircleBorder(),
+                      ),
+                      child: const Icon(Icons.flash_on),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        await controller?.flipCamera();
+                        setState(() {});
+                      },
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.all(16),
+                        shape: const CircleBorder(),
+                      ),
+                      child: const Icon(Icons.flip_camera_android),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildQrView(BuildContext context) {
+    var scanArea = (MediaQuery.of(context).size.width < 400 ||
+            MediaQuery.of(context).size.height < 400)
+        ? 150.0
+        : 300.0;
+    return QRView(
+      key: qrKey,
+      onQRViewCreated: _onQRViewCreated,
+      overlay: QrScannerOverlayShape(
+          borderColor: Colors.red,
+          borderRadius: 10,
+          borderLength: 30,
+          borderWidth: 10,
+          cutOutSize: scanArea),
+      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+    controller.scannedDataStream.listen((scanData) async {
+      setState(() {
+        result = scanData;
+        if (!cameraPaused) {
+          controller.pauseCamera();
+          cameraPaused = true;
+        }
+      });
+
+      // Make sure result!.code is not null before calling the _addRow function
+      if (result != null && result!.code != null) {
+        await _addRow(result!.code!, result!.format);
+
+        // Once the row has been added, navigate back to the main screen
+        if (mounted) {
+          // Check if the current screen is the QR scanning screen before popping
+          if (Navigator.of(context).canPop()) {
+            Navigator.of(context).pop();
+          }
+        }
+      }
+    });
+  }
+
+  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
+    log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
+    if (!p) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('no Permission')),
+      );
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+}
